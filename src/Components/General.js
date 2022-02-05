26c31de3da414b7b2d9221d527810e11e9cdb90a
William Addesso <wsaddesso@gmail.com>
2022-02-05 15:53:47 -0500

    Installed all dependencies. Set up General.js component as a controlled component.

--- src/Components/General.js
+++ src/Components/General.js
@@ -1,12 +1,68 @@
 import React, { Component } from 'react';
 
 class General extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      phoneNumber: '',
+    };
+    this.handleUserInput = this.handleUserInput.bind(this);
+  }
+
+  handleUserInput(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
   render() {
     return (
-      <div>
-        <label for="first_name">First Name</label>
-        <input type="text" id="first_name" name="first_name"></input>;
-      </div>
+      <form>
+        <label>
+          First Name:
+          <input
+            type="text"
+            name="firstName"
+            value={this.state.firstName}
+            onChange={this.handleUserInput}
+          ></input>
+        </label>
+        <br />
+
+        <label>
+          Last Name:
+          <input
+            type="text"
+            name="lastName"
+            value={this.state.lastName}
+            onChange={this.handleUserInput}
+          ></input>
+        </label>
+        <br />
+
+        <label>
+          Email:
+          <input
+            type="text"
+            name="email"
+            value={this.state.email}
+            onChange={this.handleUserInput}
+          ></input>
+        </label>
+        <br />
+
+        <label>
+          Phone Number:
+          <input
+            type="text"
+            name="phoneNumber"
+            value={this.state.phoneNumber}
+            onChange={this.handleUserInput}
+          ></input>
+        </label>
+      </form>
     );
   }
 }
