61d4bb6fbdc1babc3307e900bae4ae707ba22519
William Addesso <wsaddesso@gmail.com>
2022-02-06 21:11:59 -0500

    Added form template. Added handleUserInput function.

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -1,8 +1,75 @@
 import React, { Component } from 'react';
 
 class Work extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleUserInput = this.handleUserInput.bind(this);
+  }
+
+  handleUserInput() {
+    console.log('User Input');
+  }
+
   render() {
-    return <div>Work</div>;
+    return (
+      <div>
+        <label>
+          Job:
+          <input
+            type=""
+            name=""
+            value=""
+            onChange={this.handleUserInput}
+          ></input>
+        </label>
+        <br />
+
+        <label>
+          Position Title:
+          <input
+            type=""
+            name=""
+            value=""
+            onChange={this.handleUserInput}
+          ></input>
+        </label>
+        <br />
+
+        <label>
+          Main Tasks:
+          <input
+            type=""
+            name=""
+            value=""
+            onChange={this.handleUserInput}
+          ></input>
+        </label>
+        <br />
+
+        <label>
+          Start Date:
+          <input
+            type=""
+            name=""
+            value=""
+            onChange={this.handleUserInput}
+          ></input>
+        </label>
+        <br />
+
+        <label>
+          End Date:
+          <input
+            type=""
+            name=""
+            value=""
+            onChange={this.handleUserInput}
+          ></input>
+        </label>
+        <br />
+      </div>
+    );
   }
 }
 
