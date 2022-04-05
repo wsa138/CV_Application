7aa8fd3a587adab307eb2cc352183ee114b6504e
William Addesso <wsaddesso@gmail.com>
2022-04-04 21:39:38 -0400

    Set up functions and state to show and hide the second experience form section.

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -3,7 +3,18 @@ import React, { Component } from 'react';
 class Work extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { experienceDisplayTwoVisible: false };
+
+    this.displayExperienceFormTwo = this.displayExperienceFormTwo.bind(this);
+  }
+
+  displayExperienceFormTwo(e) {
+    e.preventDefault();
+    this.setState((prevState) => {
+      return {
+        experienceDisplayTwoVisible: !prevState.experienceDisplayTwoVisible,
+      };
+    });
   }
 
   render() {
@@ -99,6 +110,100 @@ class Work extends Component {
 
           <br />
         </form>
+
+        {/*Button adds another experience form*/}
+        <button onClick={this.displayExperienceFormTwo}>Add Experience</button>
+        {this.state.experienceDisplayTwoVisible && (
+          <form className="form-component-section container">
+            <div className="form-group row">
+              <label for="company" className="col-form-label col-sm-3">
+                Company:
+              </label>
+              <div className="col-sm-9">
+                <input
+                  className="form-control"
+                  id="company"
+                  type="text"
+                  name="company"
+                  value={workState.company}
+                  onChange={this.props.changeWork}
+                ></input>
+              </div>
+            </div>
+
+            <br />
+
+            <div className="row form-group">
+              <label for="positionTitle" className="col-form-label col-sm-3">
+                Position Title:
+              </label>
+              <div className="col-sm-9">
+                <input
+                  className="form-control"
+                  id="positionTitle"
+                  type="text"
+                  name="positionTitle"
+                  value={workState.positionTitle}
+                  onChange={this.props.changeWork}
+                ></input>
+              </div>
+            </div>
+
+            <br />
+
+            <div className="row form-group">
+              <label for="mainTasks" className="col-form-label col-sm-3">
+                Main Tasks:
+              </label>
+              <div className="col-sm-9">
+                <input
+                  className="form-control"
+                  id="mainTasks"
+                  type="text"
+                  name="mainTasks"
+                  value={workState.mainTasks}
+                  onChange={this.props.changeWork}
+                ></input>
+              </div>
+            </div>
+
+            <br />
+
+            <div className="row form-group">
+              <label for="startDate" className="col-form-label col-sm-3">
+                Start Date:
+              </label>
+              <div className="col-sm-9">
+                <input
+                  id="startDate"
+                  type="date"
+                  name="startDate"
+                  value={workState.startDate}
+                  onChange={this.props.changeWork}
+                ></input>
+              </div>
+            </div>
+
+            <br />
+
+            <div className="row form-group">
+              <label for="endDate" className="col-form-label col-sm-3">
+                End Date:
+              </label>
+              <div className="col-sm-9">
+                <input
+                  id="endDate"
+                  type="date"
+                  name="endDate"
+                  value={workState.endDate}
+                  onChange={this.props.changeWork}
+                ></input>
+              </div>
+            </div>
+
+            <br />
+          </form>
+        )}
       </div>
     );
   }
